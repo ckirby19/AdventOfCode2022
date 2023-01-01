0c0d4b8d8eba44538b26a2497e50142fdcaccc73
Conor Kirby <conorkirby1@gmail.com>
2023-01-01 18:33:27 +0000

    Added Day 14 solution, started Day 12

--- Day12/ShortestPath.py
+++ Day12/ShortestPath.py
@@ -1,11 +1,103 @@
-def findShortesPath():
-    orderedPairs = []
-    with open(txt) as f:
-        for line in f:
-            pass
+import math
+import time
+class Grid:
+    def __init__(self,txt) -> None:
+        self.txt = txt
+        self.grid = []
+        self.startPos = None # (row,column)
+        self.endPos = None
+        self.createGrid()
+        
+        self.directions = [(1,0),(-1,0),(0,1),(0,-1)]
+
+    def createGrid(self):
+        with open(self.txt) as f:
+            for i,line in enumerate(f):
+                row = list(line.strip())
+                if not self.startPos and "S" in row:
+                    startIndex = row.index("S")
+                    self.startPos = (i,startIndex)
+                    row[startIndex] = "a"
+                if not self.endPos and "E" in row:
+                    endIndex = row.index("E")
+                    self.endPos = (i,endIndex)
+                    row[endIndex] = "z"
+                
+                self.grid.append(row)
+
+    def isValidPos(self,a):
+        if a[0] < 0 or a[0] >= len(self.grid):
+            return False
+        elif a[1] < 0 or a[1] >= len(self.grid[0]):
+            return False
+        return True
+    
+    def distanceToGoal(self,a):
+        return abs(self.endPos[0] - a[0]) + abs(self.endPos[1] - a[1])
+    
+    def binaryInsert(self,value,stack,index):
+        # Insert a value into a stack, sorted (descending) by element at index 
+        left = 0
+        right = len(stack)
+        # print("Stack before:", stack)
+        while left < right:
+            middle = left + (right-left)//2
+            this = stack[middle][index]
+            if this > value[index]:
+                left = middle + 1
+            elif this < value[index]:
+                right = middle
+            else:
+                # print("stack after:",stack[:middle] + [value] + stack[middle:])
+                return stack[:middle] + [value] + stack[middle:]
+        # print("stack after:",stack[:left] + [value] + stack[left:])
+        return stack[:left] + [value] + stack[left:]
+
+    def traverseGrid(self):
+        ##checks all paths but preference for those that have the lowest score, where score = numSteps + Euclidean distanceToGoal
+        minSteps = math.inf
+        bestPath = None
+        shortestPathLengths = dict()
+        shortestPathLengths[self.startPos] = 0
+        stack = [(self.startPos,0,{self.startPos},self.distanceToGoal(self.startPos))]
+        while stack:
+            current,numSteps,currentPath,_ = stack.pop()
+            if current == self.endPos:
+                if numSteps < minSteps:
+                    minSteps = numSteps
+                    bestPath = currentPath
+            else:
+                for dir in self.directions:
+                    nextPos = (current[0] + dir[0],current[1] + dir[1])
+                    if self.isValidPos(nextPos) and nextPos not in currentPath:
+                        heightDif = ord(self.grid[nextPos[0]][nextPos[1]]) - ord(self.grid[current[0]][current[1]])
+                        if heightDif <= 1:
+                            newSteps = numSteps+1
+                            if nextPos not in shortestPathLengths.keys():
+                                shortestPathLengths[nextPos] = newSteps
+                            #Only add to stack if this is the shortest path to this location
+                            if newSteps <= shortestPathLengths[nextPos]:
+                                shortestPathLengths[nextPos] = newSteps
+                                newData = [nextPos,newSteps,currentPath.union({nextPos}),self.distanceToGoal(nextPos)+newSteps]
+                                stack = self.binaryInsert(newData,stack,1)
+            # if self.grid[current[0]][current[1]] == 'd':
+        #     print(f'Current stack has {stack} with {newSteps} steps and score of {newData[3]}')
+        # print(f'Final path is: {bestPath}')
+        return minSteps
+
+    def __str__(self) -> str:
+        print(f'Starting position is {self.startPos} with a goal of {self.endPos}. Grid is:')
+        for line in self.grid:
+            print(line)
+        
 
 
 if __name__ == "__main__":
     txt = "input.txt"
-    # print(FindOrderedPairsSum(txt))
-    print(CalculateDecoderKey(txt))
+    grid = Grid(txt)
+    start = time.perf_counter()
+    print("Started Part 1 traversal")
+    steps = grid.traverseGrid()
+    print("Number of steps =",steps)
+    end = time.perf_counter()
+    print(f"Time taken to complete Part 1 = {end - start:0.5f} seconds")
---
+++ Day14/SandUnits.py
@@ -0,0 +1,164 @@
+import math
+import time
+class SandGrid:
+    def __init__(self,txt,isPart2) -> None:
+        self.txt = txt
+        self.isPart2 = isPart2
+        self.grid = [] # (x,y) coordinates
+        self.sandStart = (500,0)
+        self.minX = 500
+        self.maxX = 500
+        self.maxY = 0
+        self.restingSand = 0
+        self.time = 0
+        self.flowing = False
+        self.createGrid()
+        
+        self.directions = [(0,1),(-1,1),(1,1)] # Sand turns to rock if cannot take any of these directions
+
+    def createGrid(self):
+        rowsWithRocks = dict() #A mapping of row: rock positions in that row
+        with open(self.txt) as f:
+            for line in f:
+                line = line.strip().split()
+                for y in range(0,len(line)-1,2):
+                    rock1 = line[y].split(",")
+                    rock2 = line[y+2].split(",")
+                    rockStartX,rockStartY = int(rock1[0]),int(rock1[1])
+                    rockEndX,rockEndY = int(rock2[0]),int(rock2[1])
+                    if rockStartX == rockEndX:
+                        #Then this straight line is in Y
+                        rockMin = min(rockStartY,rockEndY)
+                        rockMax = max(rockStartY,rockEndY)
+                        self.maxY = max(rockMax,self.maxY)
+                        
+                        for y in range(rockMin,rockMax+1):
+                            if y in rowsWithRocks.keys():
+                                rowsWithRocks[y].add(rockStartX)
+                            else:
+                                rowsWithRocks[y] = {rockStartX}
+                            # posWithRocks.add((rockStart[0],y))
+                    else:
+                        rockMin = min(rockStartX,rockEndX)
+                        rockMax = max(rockStartX,rockEndX)
+                        self.minX = min(rockMin,self.minX)
+                        self.maxX = max(rockMax,self.maxX)
+                        for x in range(rockMin,rockMax+1):
+                            if rockStartY in rowsWithRocks.keys():
+                                rowsWithRocks[rockStartY].add(x)
+                            else:
+                                rowsWithRocks[rockStartY] = {x}
+        #minX -> 0, maxX -> maxX - minX
+        for y in range(self.maxY+1):
+            toAdd = []
+            for x in range(self.minX,self.maxX+1):
+                if y in rowsWithRocks.keys():
+                    if x in rowsWithRocks[y]:
+                        # print(x,y)
+                        toAdd.append('#')
+                    else:
+                        toAdd.append('.')
+                else:
+                    toAdd.append('.')
+            self.grid.append(toAdd)
+        
+        if self.isPart2:
+            #Now we must reshape our grid slightly - We need to pad to the left and right
+            self.maxY += 2
+            padLeft = (self.maxY - (self.sandStart[0] - self.minX))
+            padRight = (self.maxY - (self.maxX - self.sandStart[0]))
+            self.minX -= padLeft
+            self.maxX += padRight
+            self.grid.append(['.']*len(self.grid[0]))
+            self.grid.append(['#']*len(self.grid[0]))
+
+            paddedGrid = []
+            for y in range(len(self.grid)):
+                toAdd = ['.']*padLeft + self.grid[y] + ['.']*padRight
+                paddedGrid.append(toAdd)
+            paddedGrid[-1] = ['#']*len(paddedGrid[0])
+            self.grid = paddedGrid
+
+    def inRockRange(self,a):
+        xBool = 0 <= a[0] < len(self.grid[0])
+        yBool = a[1] < len(self.grid)
+        return xBool and yBool
+
+    def advanceTime(self):
+        #advances until sand lands, or if sand does not land then sets the flag
+        self.time += 1
+        currentPos = (self.sandStart[0] - self.minX,0)
+        moveType = 0
+        while moveType <= 2:
+            dir = self.directions[moveType]
+            nextPos = (currentPos[0] + dir[0], currentPos[1] + dir[1])
+            # if self.time == 23:
+            #     print(currentPos,nextPos,dir)
+            if not self.inRockRange(nextPos):
+                self.flowing = True
+                break
+            elif self.grid[nextPos[1]][nextPos[0]] == '.':
+                #Then we can move here
+                currentPos = nextPos
+                moveType = 0
+            else:
+                moveType+=1
+        if not self.flowing:
+            #Then the sand has landed
+            self.restingSand += 1
+            self.grid[currentPos[1]][currentPos[0]] = 'o'
+        # print(f'Time = {self.time}, Grid is:')
+        # for line in self.grid:
+        #     print(line)
+
+    def advanceUntilBlocked(self):
+        #For part 2, with floor at bottom, this will continuously add sand until sand blocks the source
+        blocked = False
+        while blocked == False:
+            self.time += 1
+            currentPos = (self.sandStart[0] - self.minX,0)
+            moveType = 0
+            while moveType <= 2:
+                dir = self.directions[moveType]
+                nextPos = (currentPos[0] + dir[0], currentPos[1] + dir[1])
+                if self.grid[nextPos[1]][nextPos[0]] == '.':
+                    #Then we can move here
+                    currentPos = nextPos
+                    moveType = 0
+                else:
+                    moveType += 1
+            self.restingSand += 1
+            self.grid[currentPos[1]][currentPos[0]] = 'o'
+            if currentPos == (self.sandStart[0] - self.minX,0):
+                blocked = True
+
+            # print(f'Time = {self.time}, Grid is:')
+            # for line in self.grid:
+            #     print(line)
+
+    def simulate(self):
+        if self.isPart2:
+            self.advanceUntilBlocked()
+        else:
+            while self.flowing == False:
+                self.advanceTime()
+        return self.restingSand
+
+    def __str__(self):
+        for line in self.grid:
+            print(line)
+        return (f'X = {self.minX,self.maxX}, Y is {self.maxY}')
+                
+if __name__ == "__main__":
+    txt = "input.txt"
+    grid = SandGrid(txt,True)
+    start = time.perf_counter()
+    print("Started Part 2 grid creation")
+    # print(grid)
+    print("Started simulation")
+    total = grid.simulate()
+    print(total)
+    end = time.perf_counter()
+    print(f"Time taken to complete Part 1 = {end - start:0.5f} seconds")      
+
+                        
